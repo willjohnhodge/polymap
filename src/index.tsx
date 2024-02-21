6997d1c11106d5b99063665e6c6f6535f3cb55c9
willjohnhodge <willjohnhodge@gmail.com>
2024-02-21 20:37:25 +1100

    react 18 react-dom createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import App from 'App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals(sendToVercelAnalytics);
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
